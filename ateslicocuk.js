536ae106c6b92ae7a6353032427ed689fd4aa4d7
HBBA2000 <122740024+hbb200009@users.noreply.github.com>
2026-02-03 21:21:48 +0300

    Update ateslicocuk.js

--- ateslicocuk.js
+++ ateslicocuk.js
@@ -1,1 +1,10 @@
-
+function kontrolEt() {
+  var dogruSifre = "22 Aralık 2025 19:47";
+  var girilenSifre = document.getElementById("sifre").value;
+        
+  if (girilenSifre === dogruSifre) {
+    window.location.href = "main/album.html"; 
+  } else {
+    alert("Hatalı şifre girişi!");
+  }
+}
